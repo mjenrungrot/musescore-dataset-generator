fe00596b608a19e6c256875eb875c3923ca3a6a7
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-07-23 11:21:28 -0700

    add annotation generator

--- scoreAnnotationGenerator.py
+++ scoreAnnotationGenerator.py
@@ -1,6 +1,11 @@
 import music21
 from svgpathtools import svg2paths
 import pandas as pd
+import glob
+import os
+import pretty_midi
+import pdb
+from parallel_utils import parallel_process
 
 def get_annotation_data(xml_file):
   c = music21.converter.parse(xml_file)
@@ -19,7 +24,8 @@ def get_annotation_data(xml_file):
       for staff in staves:
         measures = staff.getElementsByClass('Measure')
         for measure in measures:
-          measure_num = measure.number - page.measureStart + 1
+          measure_num = measure.number
+
           if measure_num not in notehead_measures: notehead_measures[measure_num] = 0
           if measure_num not in notehead_beats: notehead_beats[measure_num] = {}
           if len(measure.voices) == 0:
@@ -48,12 +54,28 @@ def get_annotation_data(xml_file):
       'notehead_beats': notehead_beats,
     })
 
-  return data
+  cumulative_notehead_strips = []
+  notehead_measures = {}
+  notehead_beats = {}
+  for x in data:
+    cumulative_notehead_strips.extend(x['cumulative_notehead_strips'])
+    keys = x['notehead_measures'].keys()
+    for key in keys:
+      notehead_measures[key] = x['notehead_measures'][key]
+    keys = x['notehead_beats'].keys()
+    for key in keys:
+      notehead_beats[key] = x['notehead_beats'][key]
+
+  return {
+    'cumulative_notehead_strips': cumulative_notehead_strips,
+    'notehead_measures': notehead_measures,
+    'notehead_beats': notehead_beats,
+  }
 
 def get_centers(svg_file):
   paths, attributes = svg2paths(svg_file)
   pack = zip(paths, attributes)
-  filtered_pack = filter(lambda x: x[1]['class'] == 'Note', pack)
+  filtered_pack = list(filter(lambda x: x[1]['class'] == 'Note', pack))
   paths, attributes = zip(*filtered_pack)
   bboxes = list(map(lambda path: path.bbox(), paths))
   centers = list(map(lambda box: (round((box[0]+box[1])/2, 2), round((box[2]+box[3])/2, 2)), bboxes))
@@ -76,11 +98,11 @@ def splitCentersByStrips(centers, splitter_array):
 
     strips[i] = strip
     current_offset = counter
+    if current_offset == len(centers):
+      return strips, i
 
-  assert counter == len(centers), "Number of noteheads is not satisfied with the splitter array"
-
-  return strips
-
+  assert False, "Should haven't been here"
+    
 def splitCentersByMeasures(centers, splitter_dict, offset_measure=0):
   # Sort by columns
   column_sorted_centers = sorted(centers, key=lambda x: x[0])
@@ -123,7 +145,7 @@ def splitCentersByBeats(centers, splitter_dict):
       counter += 1
       if counter == splitter_dict[key]: break
     
-    beats[key] = beat
+    if len(beat) > 0: beats[key] = beat
     current_offset += counter
     total_counter += counter
     
@@ -144,22 +166,27 @@ def filterIntegerBeats(beat_locations_dict):
         beat_locations_dict[measure_key][int(beat_key)] = beat_locations_dict[measure_key].pop(beat_key)
   return beat_locations_dict
 
-def process(score_name, xml_file, svg_filepaths):
+def process(score_name, xml_file, svg_filepaths, midi_filepath):
   xml_annotations = get_annotation_data(xml_file)
-  assert len(xml_annotations) == len(svg_filepaths), "Number of pages mismatches"
+  # print(xml_file)
+  # print(len(xml_annotations), len(svg_filepaths))
+  # assert len(xml_annotations) == len(svg_filepaths), "Number of pages mismatches"
 
-  df = pd.DataFrame(columns=['score', 'measure', 'beat', 'strip', 'page', 'hpixel'])
+  df_score = pd.DataFrame(columns=['score', 'measure', 'beat', 'strip', 'page', 'hpixel'])
+  df_audio = pd.DataFrame(columns=['measure', 'time'])
 
-  for page_num in range(len(xml_annotations)):
+  offset_strips = 0
+  offset_measure = 0
+  for page_num in range(len(svg_filepaths)):
     centers = get_centers(svg_filepaths[page_num])
-    strips = splitCentersByStrips(centers, xml_annotations[page_num]['cumulative_notehead_strips'])
+    strips, offset_strip = splitCentersByStrips(centers, xml_annotations['cumulative_notehead_strips'][offset_strips:])
+    offset_strips += offset_strip
 
     # Split to measures
     measures = {}
     measure_strip_dict = {}
-    offset_measure = 0
     for strip_num in strips.keys():
-      tmp_measures, end_measure = splitCentersByMeasures(strips[strip_num], xml_annotations[page_num]['notehead_measures'], offset_measure=offset_measure)
+      tmp_measures, end_measure = splitCentersByMeasures(strips[strip_num], xml_annotations['notehead_measures'], offset_measure=offset_measure)
       offset_measure = end_measure + 1
       for key in tmp_measures.keys():
         measures[key] = tmp_measures[key]
@@ -168,7 +195,7 @@ def process(score_name, xml_file, svg_filepaths):
     # Split to beats
     beat_notes = {}
     for key in measures.keys():
-      beat_notes[key] = splitCentersByBeats(measures[key], xml_annotations[page_num]['notehead_beats'][key])
+      beat_notes[key] = splitCentersByBeats(measures[key], xml_annotations['notehead_beats'][key])
 
     # Get the mode of x-position for each beat position
     beat_locations = {}
@@ -180,10 +207,12 @@ def process(score_name, xml_file, svg_filepaths):
     # Filter for only integer-number beats
     beat_locations = filterIntegerBeats(beat_locations)
     
+    # print(measures.keys())
+
     # Update dataframe
     for measure_key in measures.keys():
       for beat_key in beat_locations[measure_key].keys():
-        df = df.append({
+        df_score = df_score.append({
           'score': score_name,
           'measure': measure_key,
           'beat': beat_key,
@@ -192,8 +221,45 @@ def process(score_name, xml_file, svg_filepaths):
           'hpixel': beat_locations[measure_key][beat_key],
         }, ignore_index=True)
 
-  return df
+  mid = pretty_midi.PrettyMIDI(midi_filepath)
+  beats = mid.get_beats()
 
-df = process('sample', 'Final_Project.xml', ['Final_Project-1.svg', 'Final_Project-2.svg'])
+  assert len(beats) % len(xml_annotations['notehead_measures'].keys()) == 0
 
+  beat_per_measure = len(beats) // len(xml_annotations['notehead_measures'].keys())
+  for i in range(len(beats)):
+    measure_num = 1 + (i // beat_per_measure)
+    beat_num = 1 + (i % beat_per_measure)
+    df_audio = df_audio.append({
+      'measure': '{:}.{:}'.format(measure_num, beat_num),
+      'time': beats[i]
+    }, ignore_index=True)
 
+  return df_score, df_audio
+
+def f(xml_path):
+  filename = os.path.splitext(os.path.basename(xml_path))[0]
+  svg_paths = glob.glob('svg/{:}*.svg'.format(filename))
+
+  # Sort by pages
+  svg_paths = sorted(svg_paths, key=lambda x: int(x.replace('.','-').split('-')[1]))
+
+  # midi file
+  midi_path = os.path.join('midi', filename + '.mid')
+
+  # Convert to annotation
+  df_score, df_audio = process(filename, xml_path, svg_paths, midi_path)
+
+  # Write to csv 
+  df_score.to_csv(os.path.join('score_annot', '{:}_beats.csv'.format(filename)), index=False)
+  df_audio.to_csv(os.path.join('audio_annot', '{:}.csv'.format(filename)), index=False)
+
+if not os.path.exists('score_annot'):
+  os.mkdir('score_annot')
+
+if not os.path.exists('audio_annot'):
+  os.mkdir('audio_annot')
+
+xml_paths = glob.glob('xml/*.xml')
+parallel_process(xml_paths, f, front_num=0)
+# f(xml_paths[5])
