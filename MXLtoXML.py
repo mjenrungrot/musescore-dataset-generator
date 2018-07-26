62747ae1fdcd254a1b9b24e6740f9c1f58dfe074
Teerapat Jenrungrot <mjenrungrot@hmc.edu>
2018-07-26 14:09:27 -0700

    lint MXLtoXML

--- MXLtoXML.py
+++ MXLtoXML.py
@@ -1,25 +1,37 @@
+# pylint: disable=invalid-name
+"""
+MXLtoXML is a Python script for converting all MXL files to XML files.
+"""
 import glob
 import os
-import tqdm
+import subprocess
 import lxml.etree
 from parallel_utils import parallel_process
 
-# Set environment variable to prevent QXcbConnection: Could not connect to display problem
+# Set environment variable to prevent QXcbConnection: Could not connect to
+# display problem
 os.environ['QT_QPA_PLATFORM'] = 'offscreen'
 
 if not os.path.exists('xml'):
-  os.mkdir('xml') 
+    os.mkdir('xml')
+
 
 def f(mxl_path):
-  filename = os.path.splitext(os.path.basename(mxl_path))[0]
-  os.system("musescore {:} -r 300 -f -o xml/{:}.xml".format(mxl_path, filename))
-  try:
-    lxml.etree.parse("xml/{:}.xml".format(filename))
-  except lxml.etree.XMLSyntaxError:
-    os.remove("xml/{:}.xml".format(filename))
-  
-  return 0
+    """
+    This function converts a single MXL file to a single XML file.
+    The XML invalid file is ignored.
+    """
+    filename = os.path.splitext(os.path.basename(mxl_path))[0]
+    subprocess.call(
+        "musescore {:} -r 300 -f -o xml/{:}.xml".format(mxl_path, filename),
+        shell=False)
+    try:
+        lxml.etree.parse("xml/{:}.xml".format(filename)) # pylint: disable=c-extension-no-member
+    except lxml.etree.XMLSyntaxError: # pylint: disable=c-extension-no-member
+        os.remove("xml/{:}.xml".format(filename))
+
+    return 0
+
 
 mxl_paths = glob.glob('mxl/*.mxl')
 parallel_process(mxl_paths, f)
-
